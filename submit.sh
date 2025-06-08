bd9ddcd5b0ea4ac3275b9b0472f4243dadc08235
wexther <923977588@qq.com>
2025-06-08 12:05:24 +0800

    新submit

--- submit.sh
+++ submit.sh
@@ -1,7 +1,14 @@
-cd ..
+Cargo build --release
+
+rm -f -r simple_db
+
+mkdir simple_db
+
+cp src simple_db/src -r
+cp target/release/simple_db simple_db/target/release/simple_db
+cp Cargo.toml simple_db/Cargo.toml
+cp Cargo.lock simple_db/Cargo.lock
 
 zip submit.zip -r simple_db/src simple_db/target/release/simple_db simple_db/Cargo.toml simple_db/Cargo.lock
 
-mv submit.zip simple_db/submit.zip
-
-cd simple_db
+rm -f -r simple_db
