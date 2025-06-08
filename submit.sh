36fcfc9b6aaa60ec42d74a000cbbf1407bdb22d7
wexther <923977588@qq.com>
2025-06-08 12:29:35 +0800

    修改提交脚本

--- submit.sh
+++ submit.sh
@@ -1,14 +1,11 @@
-Cargo build --release
+cargo build --release
 
-rm -f -r simple_db
-
-mkdir simple_db
-
+mkdir simple_db 
 cp src simple_db/src -r
+mkdir simple_db/target
+mkdir simple_db/target/release
 cp target/release/simple_db simple_db/target/release/simple_db
 cp Cargo.toml simple_db/Cargo.toml
 cp Cargo.lock simple_db/Cargo.lock
 
-zip submit.zip -r simple_db/src simple_db/target/release/simple_db simple_db/Cargo.toml simple_db/Cargo.lock
-
-rm -f -r simple_db
+zip -FS submit.zip -rm simple_db
